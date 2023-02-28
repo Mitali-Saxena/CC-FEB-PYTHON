a94311fd92fcecbe23a143c55721f8bf2b831080
Mitali-Saxena <mitalisaxena0477@gmail.com>
2023-02-28 18:24:27 +0530

    url shortener
    Commit fourth project

---
+++ TASK1-url_shortener_project/shortener_app/main.py
@@ -0,0 +1,67 @@
+
+import validators
+
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+from starlette.datastructures import URL
+from .config import get_settings
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+        "administration info", secret_key=db_url.secret_key
+    )
+
+def raise_not_found (request):
+    message = f"URL '{request.url}' doesn't exist"
+    raise HTTPException(status_code=404, detail=message)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()    
+
+ #..
+@app.get(
+    "/admin/{secret_key}",
+    name="administration info",
+    response_model= schemas.URLInfo,
+    )
+def get_url_info(
+    secret_key: str, request: Request, db: Session = Depends(get_db)
+):
+
+    if db_url := crud.get_db_url_by_key(db, secret_key=secret_key):
+        db_url.url = db_url.key
+        db_url.admin_url = db_url.secret_key
+        return db_url
+    else:
+        raise_not_found(request)    
+
+# ...
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
+# ...
+
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided URL is not valid")
+
+    db_url = crud.create_db_url(db=db, url=url)
+    db_url.url = db_url.key
+    db_url.admin_url = db_url.secret_key
+
+    return db_url
+
